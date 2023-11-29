8296562d2cb1580cb06acb841d50b9abfd342a0b
Nacho <146840051+rivero04@users.noreply.github.com>
2023-11-29 09:55:16 -0300

    Tasks for hash_tables

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i = 0;
 	hash_table_t *hash_table = NULL;
 
-	hash_table = malloc(sizeof(hash_node_t));
+	hash_table = malloc(sizeof(hash_table_t));
 
 	if (hash_table == NULL)
 		return (NULL);
