150298bac638096220559c52198952fa2da7dce5
Nacho <146840051+rivero04@users.noreply.github.com>
2023-11-30 11:28:44 -0300

    Tasks for hash_tables

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -7,26 +7,26 @@
   */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i = 0;
-    hash_node_t *current_node;
-    char aux = 0;
+	unsigned long int i = 0;
+	hash_node_t *current_node;
+	char aux = 0;
 
-    if (ht == NULL || ht->array == NULL)
-        return;
-    printf("{");
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
 
-    for (i = 0; i < ht->size; i++)
-    {
-        current_node = ht->array[i];
+	for (i = 0; i < ht->size; i++)
+	{
+		current_node = ht->array[i];
 
-        while (current_node != NULL)
-        {
-            if (aux == 1)
-                printf(", ");
-            printf("'%s': '%s'", current_node->key, current_node->value);
-            aux = 1;
-            current_node = current_node->next;
-        }
-    }
-    printf("}\n");
+		while (current_node != NULL)
+		{
+			if (aux == 1)
+				printf(", ");
+			printf("'%s': '%s'", current_node->key, current_node->value);
+			aux = 1;
+			current_node = current_node->next;
+		}
+	}
+	printf("}\n");
 }
