6c94bb1a4fb52184467f4d573f80afca967bba4c
Nacho <146840051+rivero04@users.noreply.github.com>
2023-11-30 11:36:02 -0300

    Tasks for hash_tables

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,48 @@
+#include "hash_tables.h"
+/**
+  * hash_table_delete - Deletes a hash table
+  * @ht: The hash table to frees
+  *
+  * Return: ...
+  */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *node = NULL, *current_node = NULL;
+
+if (ht && ht->size && ht->array)
+	{
+		for (; i < ht->size; ++i)
+		{
+			node = ht->array[i];
+
+			if (node)
+			{
+				if (node->next)
+				{
+					node = node->next;
+					while (node)
+					{
+						current_node = node;
+						node = node->next;
+						free(current_node->key);
+						free(current_node->value);
+						free(current_node);
+					}
+				}
+
+				node = ht->array[i];
+				if (node->key && node->value)
+				{
+					free(node->key);
+					free(node->value);
+				}
+			}
+
+			free(node);
+		}
+
+		free(ht->array);
+		free(ht);
+	}
+}
